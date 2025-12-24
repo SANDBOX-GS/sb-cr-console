02472aa851e0931bc9b30f61b9d25cef573e2a3e
kibbeum <kibbeum@sandboxnetwork.net>
2025-12-24 16:51:30 +0900

    style: 기타페이지 UI 작업

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,69 +1,75 @@
 "use client";
 
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 import svgPaths from "../imports/svg-dfaq4i8l7r";
+import Image from "next/image";
+import { IMG_URL } from "@/constants/dbConstants";
 
 export function Footer() {
     return (
-        <motion.div
-            initial={{ y: 20, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{ duration: 0.5, delay: 0.2 }}
-            className="bg-white/80 backdrop-blur-xl relative shrink-0 w-full border-t border-slate-200/50"
-        >
-            <div className="flex flex-row items-center overflow-clip relative size-full">
-                <div className="box-border content-stretch flex items-center justify-between px-8 py-4 relative w-full">
-                    <div className="content-stretch flex gap-5 items-center justify-start relative shrink-0">
-                        <div className="flex items-center justify-center relative shrink-0">
-                            <div className="flex-none scale-y-[-100%]">
-                                <div className="h-8 relative w-[46.233px]">
-                                    <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 47 32">
-                                        <path d={svgPaths.p2b99d040} fill="var(--fill-0, #64748B)" />
-                                    </svg>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="content-stretch flex flex-col font-['Pretendard:Regular',_sans-serif] items-start justify-center leading-[0] not-italic relative shrink-0 text-[12px] text-nowrap text-slate-400">
-                            <div className="content-stretch flex gap-2 items-center justify-start relative shrink-0">
-                                <div className="relative shrink-0">
-                                    <p className="leading-[1.6] text-nowrap whitespace-pre">(주)샌드박스네트워크</p>
-                                </div>
-                                <div className="relative shrink-0">
-                                    <p className="leading-[1.6] text-nowrap whitespace-pre">서울시 용산구 서빙고로 17 센트럴타워 28~30F</p>
-                                </div>
-                                <div className="relative shrink-0">
-                                    <p className="leading-[1.6] text-nowrap whitespace-pre">사업자등록번호 : 220-88-89136</p>
-                                </div>
-                            </div>
-                            <div className="relative shrink-0">
-                                <p className="leading-[1.6] text-nowrap whitespace-pre">© 2025. SANDBOX NETWORK Inc. All Rights Reserved</p>
-                            </div>
-                        </div>
+        <div className="px-6 py-4 flex flex-col md:flex-row items-center justify-between size-full bg-white shadow shadow-sky-900/15 shadow-[0_-4px_16px]">
+            <div className="flex flex-col md:flex-row items-start md:items-center gap-4 w-full">
+                <Image
+                    src={`${IMG_URL}/common/symbol_gray_46.png`}
+                    width="46"
+                    height="32"
+                />
+                <div>
+                    <div className="flex flex-col md:flex-row md:gap-2">
+                        <p className="text-sm text-neutral-500">
+                            (주)샌드박스네트워크 서울시 용산구 서빙고로 17
+                            센트럴타워 28~29F
+                        </p>
+                        <p className="text-sm text-neutral-500">
+                            사업자등록번호 : 220-88-89136
+                        </p>
                     </div>
-                    <div className="content-stretch flex flex-col items-end justify-center relative shrink-0">
-                        <div className="content-stretch flex gap-4 items-start justify-start leading-[0] not-italic relative shrink-0 text-[13px] text-nowrap text-slate-500 w-full">
-                            <div className="font-['Pretendard:Regular',_sans-serif] relative shrink-0 hover:text-slate-700 transition-colors duration-200 cursor-pointer">
-                                <p className="leading-[1.6] text-nowrap whitespace-pre">공지사항</p>
-                            </div>
-                            <div className="font-['Pretendard:Regular',_sans-serif] relative shrink-0 hover:text-slate-700 transition-colors duration-200 cursor-pointer">
-                                <p className="leading-[1.6] text-nowrap whitespace-pre">이용약관</p>
-                            </div>
-                            <div className="font-['Pretendard:Medium',_sans-serif] relative shrink-0 hover:text-slate-700 transition-colors duration-200 cursor-pointer">
-                                <p className="leading-[1.6] text-nowrap whitespace-pre">개인정보처리방침</p>
-                            </div>
-                        </div>
-                        <div className="content-stretch flex gap-1 items-center justify-center relative shrink-0">
-                            <svg className="size-3" fill="none" viewBox="0 0 12 12">
-                                <path d={svgPaths.pcd45380} stroke="var(--stroke-0, #94A3B8)" strokeLinecap="round" strokeLinejoin="round" />
-                                <path d={svgPaths.p9deeb00} stroke="var(--stroke-0, #94A3B8)" strokeLinecap="round" strokeLinejoin="round" />
-                            </svg>
-                            <div className="font-['Pretendard:Regular',_sans-serif] leading-[0] not-italic relative shrink-0 text-[12px] text-nowrap text-slate-400">
-                                <p className="leading-[1.6] whitespace-pre">contact@sandboxnetwork.net</p>
-                            </div>
-                        </div>
-                    </div>
+                    <p className="text-sm text-neutral-400">
+                        © 2026. SANDBOX NETWORK Inc. All Rights Reserved
+                    </p>
                 </div>
             </div>
-        </motion.div>
+            <div className="flex flex-col items-start md:items-center justify-start md:justify-end w-full md:max-w-[280px] pt-6 md:pt-0">
+                <div className="flex items-start justify-start md:justify-end md:items-center gap-6 w-full ">
+                    <a
+                        href="/notice"
+                        className="cursor-pointer hover:text-sky-500 active:text-sky-700 text-sm text-neutral-600"
+                    >
+                        공지사항
+                    </a>
+                    <a
+                        href="/legal/terms"
+                        className="cursor-pointer hover:text-sky-500 active:text-sky-700 text-sm text-neutral-600"
+                    >
+                        이용약관
+                    </a>
+                    <a
+                        href="/legal/privacy"
+                        className="cursor-pointer hover:text-sky-500 active:text-sky-7000 text-sm text-neutral-600"
+                    >
+                        개인정보처리방침
+                    </a>
+                </div>
+                <div className="flex items-center gap-2 w-full justify-start md:justify-end">
+                    <svg className="size-3" fill="none" viewBox="0 0 12 12">
+                        <path
+                            d={svgPaths.pcd45380}
+                            stroke="var(--stroke-0, #94A3B8)"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                        />
+                        <path
+                            d={svgPaths.p9deeb00}
+                            stroke="var(--stroke-0, #94A3B8)"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                        />
+                    </svg>
+                    <p className="text-sm text-neutral-500">
+                        contact@sandboxnetwork.net
+                    </p>
+                </div>
+            </div>
+        </div>
     );
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,12 @@
 "use client";
 
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 import { useRouter } from "next/navigation";
 import { useAuth } from "@/contexts/AuthContext";
+import Image from "next/image";
+import { IMG_URL } from "@/constants/dbConstants";
 
-export function Header () {
+export function Header() {
     const router = useRouter();
     const { isLoggedIn, logout, isLoading } = useAuth();
 
@@ -15,7 +17,7 @@ export function Header () {
             logout();
         } else {
             console.log("Not logged in, navigating to /login");
-            router.push('/login');
+            router.push("/login");
         }
     };
 
@@ -24,24 +26,35 @@ export function Header () {
             initial={{ y: -20, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             transition={{ duration: 0.5 }}
-            className="bg-white/80 backdrop-blur-xl h-[66px] relative shrink-0 w-full border-b border-slate-200/50"
+            className="bg-white backdrop-blur-xl h-[66px] relative shrink-0 w-full border-b border-slate-200/50 shadow-sky-900/15 shadow-[0_4px_16px]"
         >
             <div className="flex flex-row items-center overflow-clip relative size-full">
                 <div className="box-border content-stretch flex h-[66px] items-center justify-between px-8 py-4 relative w-full">
-                    <div className="bg-center bg-cover bg-no-repeat h-5 shrink-0 w-[162px]" style={{ backgroundImage: `url('/logo.svg')` }} />
-                    <div className="content-stretch flex gap-10 items-center justify-start relative shrink-0">
-                        <div className="box-border content-stretch flex gap-2.5 h-8 items-center justify-center px-3 py-1 relative rounded-lg shrink-0 hover:bg-slate-100/80 transition-all duration-200 cursor-pointer">
-                            <div
-                                className="box-border content-stretch flex gap-2.5 h-8 items-center justify-center px-3 py-1 relative rounded-lg shrink-0 hover:bg-slate-100/80 transition-all duration-200 cursor-pointer"
-                                onClick={handleAuthClick}
-                            >
-                                <div className="content-stretch flex gap-1 items-center justify-start relative shrink-0">
-                                    <div className="font-['Pretendard:Medium',_sans-serif] leading-[0] not-italic relative shrink-0 text-[13px] text-center text-nowrap text-slate-700">
-                                        <p className="leading-[1.6] whitespace-pre">
-                                            {isLoading ? '확인 중...' : (isLoggedIn ? '로그아웃' : '로그인')}
-                                        </p>
-                                    </div>
-                                </div>
+                    <a href="/">
+                        <Image
+                            src={`${IMG_URL}/common/logo_horizontal.png`}
+                            width="162"
+                            height="20"
+                            className="hidden md:block"
+                        />
+                        <Image
+                            src={`${IMG_URL}/common/symbol_40.png`}
+                            width="29"
+                            height="20"
+                            className="md:hidden"
+                        />
+                    </a>
+                    <div className="content-stretch flex gap-10 items-centerㄴ justify-start relative shrink-0">
+                        <div
+                            className="text-sm box-border content-stretch flex gap-2.5 h-8 items-center justify-center md:px-3 py-1 relative rounded-lg shrink-0 hover:bg-slate-100/80 transition-all duration-200 cursor-pointer"
+                            onClick={handleAuthClick}
+                        >
+                            <div className="content-stretch flex gap-1 items-center justify-start relative shrink-0">
+                                {isLoading
+                                    ? "확인 중..."
+                                    : isLoggedIn
+                                    ? "로그아웃"
+                                    : "로그인"}
                             </div>
                         </div>
                     </div>
