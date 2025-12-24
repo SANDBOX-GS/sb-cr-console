aaf491d41db0cdb432a1e379d4703376c5ae1a9f
kibbeum <kibbeum@sandboxnetwork.net>
2025-12-24 16:51:30 +0900

    style: 기타페이지 UI 작업

---
+++ app/page.jsx
@@ -0,0 +1,63 @@
+"use client";
+import { IconCard } from "@/components/common/IconCard";
+import React from "react";
+import { DocumentQuestion } from "@/components/icon/DocumentQuestion";
+import { Button } from "@/components/common/Button";
+import { useRouter } from "@/hooks/useRouter";
+import { motion } from "framer-motion";
+import { IMG_URL } from "@/constants/dbConstants";
+
+import { useAuth } from "@/contexts/AuthContext";
+import Image from "next/image";
+
+const Index = () => {
+    const { navigate } = useRouter();
+    const { isLoggedIn } = useAuth();
+    console.log(isLoggedIn);
+    const navigateToStart = () => {
+        if (isLoggedIn) {
+            navigate("/payee-info/view");
+        } else {
+            navigate("/payee-info/register");
+        }
+    };
+    return (
+        <div className="w-full h-full flex flex-col items-center justify-center mt-40 gap-8">
+            <motion.div
+                initial={{ y: 30, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.6 }}
+                className="text-center mb-12 flex flex-col items-center gap-6"
+            >
+                <Image
+                    src={`${IMG_URL}/common/symbol_120.png`}
+                    width={120}
+                    height={83}
+                />
+                <h1 className="text-slate-800">
+                    샌드박스 크리에이터
+                    <br /> 정산 시스템
+                </h1>
+                <p>
+                    크리에이터 정산 시스템에 오신 것을 환영합니다.
+                    <br />
+                    정산 시스템은 샌드박스와 함께 협업하고 있는
+                    <br /> 파트너 크리에이터분들만 접근이 가능한 프라이빗
+                    플랫폼입니다.
+                </p>
+            </motion.div>
+            <motion.div
+                initial={{ y: 30, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.6 }}
+                className="w-[240px]"
+            >
+                <Button onClick={navigateToStart} className="w-full">
+                    시작하기
+                </Button>
+            </motion.div>
+        </div>
+    );
+};
+
+export default Index;
