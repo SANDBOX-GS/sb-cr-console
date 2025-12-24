25eb10daf2c141e8576e1dc6c209eac0e1f5a3bf
kibbeum <kibbeum@sandboxnetwork.net>
2025-12-24 16:51:58 +0900

    feat: 노션임베드(react-notion-x)

---
+++ app/legal/privacy/page.jsx
@@ -0,0 +1,15 @@
+import { getNotionPage } from "@/lib/notion";
+import { NotionPage } from "@/components/common/NotionPage";
+import { NOTION_PAGE_ID } from "@/constants/dbConstants";
+
+export const revalidate = 60 * 60; // 1h
+
+export default async function PrivacyPage() {
+    const recordMap = await getNotionPage(NOTION_PAGE_ID.PRIVACY);
+
+    return (
+        <main className="mx-auto max-w-3xl px-4 py-10">
+            <NotionPage recordMap={recordMap} />
+        </main>
+    );
+}
